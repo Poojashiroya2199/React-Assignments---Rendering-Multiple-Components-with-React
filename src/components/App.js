0fffeb1636eab710e25bc7e38fd866ec3dcf1e5c
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-11-07 15:34:57 +0530

    abc

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,10 @@ const App = () => {
   return (
     <>
       <h1 data-ns-test="project-name">MovieList</h1>
-      <description data-ns-test="project-name">
+      <div data-ns-test="project-name">
         This app gives you the features to like the movie, add to watchlist, or
         remove from movielist.
-      </description>
+      </div>
     </>
   );
 };
