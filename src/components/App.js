5cbe9bfcac6fbf73b21f6b99cebd5cd0b5944a1b
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-11-07 15:16:50 +0530

    c

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React from "react";
+import "../styles/App.css";
+//import Title from "./Title";
+//import Description from "./Description";
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">MovieList</h1>
+      <p data-ns-test="project-name">
+        This app gives you the features to like the movie, add to watchlist, or
+        remove from movielist.
+      </p>
+    </>
+  );
+};
 
 export default App;
